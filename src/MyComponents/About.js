aae7aedf05159d36b7064a0b77115fdeeba4ab1a
HuligeshBondade <107861136+HuligeshBondade@users.noreply.github.com>
2023-12-07 23:54:03 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      
+      <h4>Hello Guys!!</h4>
+      <p>I am Huligesh Bondade from GM Institute of Technology, Davangere [Information Science Branch]. I created this todo list with help of youtube hands on tutorial on react. React is frontend framework of javascript basically used to build single-page application </p>
+    </div>
+  )
+}
+
+
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Footer = () => {
+
+
+  return (
+    <div className='bg-dark text-light py-3' >
+      <p className="text-center">
+       Copywrite &copy; MyTodosList.com
+      </p>
+     
+    </div>
+  )
+}
+
